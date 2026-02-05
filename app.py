e01e5b008a609992001eaf7f874835214ee4ae7d
DanielC966 <danielc966@nycstudents.net>
2026-02-05 09:17:11 -0500

    boooom

---
+++ app.py
@@ -0,0 +1,18 @@
+# Number Guess Project
+
+import random
+
+randomNumber = random.randrange(1, 11)
+guess = 0
+guess_history = []
+while guess != randomNumber:
+    guess = int(input("Guess: "))
+    if guess > randomNumber:
+        print("Your guess is GREATER than the number")
+        guess_history.append(guess)
+    elif guess < randomNumber:
+        print("Your guess is LESS than the number")
+        guess_history.append(guess)
+
+print(f"your guess is correctamundo, the number was {randomNumber}")
+print("Guess history: " + str(guess_history))

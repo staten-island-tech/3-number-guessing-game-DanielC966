1a0f55619bca438f3c838127119fd1a1c15034f6
DanielC966 <danielc966@nycstudents.net>
2026-02-06 09:06:12 -0500

    finfal

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 randomNumber = random.randrange(1, 11)
 guess = 0
 guess_history = []
+used_numbers = []
 while guess != randomNumber:
     guess = int(input("Guess: "))
     if guess > randomNumber:
@@ -13,8 +14,10 @@ while guess != randomNumber:
     elif guess < randomNumber:
         print("Your guess is LESS than the number")
         guess_history.append(guess)
+    used_numbers.append(guess)
+    print(f"Used numbers: {used_numbers}")
 
 print(f"your guess is correctamundo, the number was {randomNumber}")
-print("Guess history: \n")
+print("Guess history:")
 for i in guess_history:
-    print(guess_history[i])
+    print(f"    Incorrect guess: {i}")
